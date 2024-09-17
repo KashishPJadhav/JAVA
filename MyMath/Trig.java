56f4a1333717f7143a43ae168d144e9888a372e3
KashishPJadhav <161619524+KashishPJadhav@users.noreply.github.com>
2024-09-17 13:57:37 +0530

    Create Trig.java

---
+++ MyMath/Trig.java
@@ -0,0 +1,33 @@
+package MyMath;
+
+public class Trig {
+    private double angle;
+
+    public Trig(double angle) {
+        this.angle = Math.toRadians(angle); // Convert degrees to radians
+    }
+
+    public double getSine() {
+        return Math.sin(angle);
+    }
+
+    public double getCosine() {
+        return Math.cos(angle);
+    }
+
+    public double getTangent() {
+        return Math.tan(angle);
+    }
+
+    public double getSecant() {
+        return 1 / Math.cos(angle);
+    }
+
+    public double getCosecant() {
+        return 1 / Math.sin(angle);
+    }
+
+    public double getCotangent() {
+        return 1 / Math.tan(angle);
+    }
+}
