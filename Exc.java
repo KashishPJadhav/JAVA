27bcf7193b0bc90a7e06c0a5ebceceed08906225
Kashish Pradeep Jadhav <161619524+KashishPJadhav@users.noreply.github.com>
2025-01-22 21:18:21 +0530

    Update Exc.java

--- Exc.java
+++ Exc.java
@@ -1,20 +1,20 @@
-/*Define a new exception, called ExceptionLineTooLong, that prints out the error message "The
-strings is too long". Write a program that reads a String from user and calculates its length and throws
-an exception of type ExceptionLineTooLong in the case where a string of length is more than 80
-characters. */
-public class Exc extends Exception{
-    String s;
-    public ExceptionLineTooLong(string s)
-    {
-        this.s=s;
+public class ExceptionLineTooLong extends Exception {
+    private String invalidValue;
+
+    // Constructor to initialize the invalid value
+    public ExceptionLineTooLong(String invalidValue) {
+        super("The string is too long.");
+        this.invalidValue = invalidValue;
     }
-    public int getInvalidValue() {
-		return s;
-	}
-	public void setInvalidValue(int invalidValue) {
-		this.s = s;
+
+    // Getter for invalid value
+    public String getInvalidValue() {
+        return invalidValue;
     }
+
+    // Overridden getMessage() to include custom message
+    @Override
     public String getMessage() {
-		return String.format("String too long %s",s); 
-	}
+        return String.format("String too long: %s", invalidValue);
+    }
 }
