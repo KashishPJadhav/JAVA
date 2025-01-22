c4929be2aeeb876b4d91c65f2bb84b183b47300b
Kashish Pradeep Jadhav <161619524+KashishPJadhav@users.noreply.github.com>
2025-01-22 21:17:26 +0530

    Update Ass3_4.java

--- Ass3_4.java
+++ Ass3_4.java
@@ -1,19 +1,25 @@
 import java.util.Scanner;
 
-public class Ass3_4
-{
-    public static void main(String[] args)
-    {
-        Scanner sc=new Scanner(System.in);
-        try{
-            string st=new string();
-            String s=sc.nextLine();
-            st.sets(s);
-        }
-        catch(ExceptionLineTooLong e)
-        {
-            System.out.println("Invalid value " + e.getInvalidValue());
-            System.out.println("Message : "+e.getMessage());
+public class Ass3_4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Create an instance of StringValidator
+        StringValidator st = new StringValidator();
+
+        try {
+            System.out.println("Enter a string:");
+            String s = sc.nextLine();
+
+            // Validate the string
+            st.setString(s);
+            System.out.println("String accepted: " + st.getString());
+        } catch (ExceptionLineTooLong e) {
+            // Handle custom exception
+            System.out.println("Invalid value: " + e.getInvalidValue());
+            System.out.println("Message: " + e.getMessage());
+        } finally {
+            sc.close();
         }
     }
 }
