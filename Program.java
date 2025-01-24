39c1e3e1118b470270db85cdbaad8a212e83bfab
Kashish Pradeep Jadhav <161619524+KashishPJadhav@users.noreply.github.com>
2025-01-24 23:17:20 +0530

    Add files via upload

---
+++ Program.java
@@ -0,0 +1,20 @@
+package com.sunbeam;
+
+public class Program {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+		Program.main(10, 20);
+		Program.main();
+
+	}
+	public static void main(int a, int b) {
+		System.out.println("Hello world1");
+
+	}
+	public static void main() {
+		System.out.println("Hello world2");
+
+	}
+
+}
